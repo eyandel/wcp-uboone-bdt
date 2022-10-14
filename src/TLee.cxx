acbdf79da064ffc6b75f8d739cfbb5b0390a93dd
eyandel <eyandel16@gmail.com>
2022-10-14 11:34:53 -0500

    make work for no detvar

--- src/TLee.cxx
+++ src/TLee.cxx
@@ -2721,6 +2721,8 @@ void TLee::Set_Spectra_MatrixCov()
 
   map<int, TString>map_detectorfile_str;
 
+  if(flag_syst_detector){
+
   map_detectorfile_str[1] = detector_directory+"cov_LYDown.root";
   map_detectorfile_str[2] = detector_directory+"cov_LYRayleigh.root";
   map_detectorfile_str[3] = detector_directory+"cov_Recomb2.root";
@@ -2731,6 +2733,8 @@ void TLee::Set_Spectra_MatrixCov()
   map_detectorfile_str[8] = detector_directory+"cov_WMX.root";
   map_detectorfile_str[9] = detector_directory+"cov_WMYZ.root";
   map_detectorfile_str[10]= detector_directory+"cov_LYatt.root";
+
+}
 
   map<int, TFile*>map_file_detector_frac;
   map<int, TMatrixD*>map_matrix_detector_frac;
