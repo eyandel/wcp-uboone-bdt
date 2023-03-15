544e493209cc52c69ad8a7622a71716b0ab2be70
eyandel <eyandel16@gmail.com>
2023-03-15 15:39:56 -0500

    debug sp overlay

--- inc/WCPLEEANA/cuts.h
+++ inc/WCPLEEANA/cuts.h
@@ -3878,10 +3878,10 @@ bool LEEana::get_cut_pass(TString ch_name, TString add_cut, bool flag_data, Eval
               !map_cuts_flag["SPNumuCCSig"]) return true;
             return false;
   }else if (ch_name == "single_photon_spoverlay"){
-            if (flag_singlephoton_sel &&
+            if (flag_singlephoton_sel /*&&
               (map_cuts_flag["SPNCDeltaSig"] || map_cuts_flag["SPOutFVSig"] ||
               map_cuts_flag["SPNCPi0Sig"] || map_cuts_flag["SPNCOtherSig"] ||
-              map_cuts_flag["SPNumuCCSig"])) return true;
+              map_cuts_flag["SPNumuCCSig"])*/) return true;
             return false;
   }else if (ch_name == "single_photon_eff_spoverlay"){
             if (flag_singlephoton_eff_sel &&
