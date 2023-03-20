1f68a5b6c5762a4209add7fb890105142e31b85d
eyandel <eyandel16@gmail.com>
2023-03-20 10:02:45 -0500

    double counting pot?

--- apps/pot_counting_mc.cxx
+++ apps/pot_counting_mc.cxx
@@ -41,12 +41,22 @@ int main(int argc, char** argv){
   std::cout << "Trigger: " << T_eval->GetEntries() << std::endl;
   double total_pot = 0;
   double pot_tor875;
+  //Erin
+  int r = 0, s = 0;
   T_pot->SetBranchAddress("pot_tor875",&pot_tor875);
+  T_pot->SetBranchAddress("runNo",&r);
+  T_pot->SetBranchAddress("subRunNo",&r);
+
+  std::map<std::pair<int, int>, bool >  already_seen;
+
   for (Int_t i=0;i!=T_pot->GetEntries();i++){
     T_pot->GetEntry(i);
+    auto it = already_seen.find(std::make_pair(r,s));
+    if (it != already_seen.end()) continue;
+    already_seen[std::make_pair(r,s)] = true;
     total_pot += pot_tor875;
   }
   std::cout << "POT: " << total_pot << std::endl;
-  
+
   return 0;
 }
