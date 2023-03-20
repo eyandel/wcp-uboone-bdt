a34f0f94be8b272f79e099afa18ed8f588740c28
eyandel <eyandel16@gmail.com>
2023-03-20 11:26:55 -0500

    test shower mother

--- inc/WCPLEEANA/cuts.h
+++ inc/WCPLEEANA/cuts.h
@@ -3757,7 +3757,8 @@ bool LEEana::get_cut_pass(TString ch_name, TString add_cut, bool flag_data, Eval
     else return false;
   }else if (ch_name == "generic_nu_spoverlay"){
             if (flag_generic &&
-              (eval.match_completeness_energy>0.1*eval.truth_energyInside && pfeval.truth_single_photon==1)) return true;
+              (eval.match_completeness_energy>0.1*eval.truth_energyInside && pfeval.truth_single_photon==1
+              && (pfeval.truth_showerMother==111 || pfeval.truth_NCDelta==1))) return true;
             return false;
   }else if (ch_name == "generic_nu_overlay_sp_BG"){
             if (flag_generic &&
