69d21f73d827d6cebf0c797d510ce552eec7b0bd
eyandel <eyandel16@gmail.com>
2023-06-13 09:31:16 -0500

    debug det syst

--- src/TLee.cxx
+++ src/TLee.cxx
@@ -2834,7 +2834,7 @@ void TLee::Set_Spectra_MatrixCov()
 
    map_detectorfile_str[1] = detector_directory+"cov_LYDown.root";
    map_detectorfile_str[2] = detector_directory+"cov_LYRayleigh.root";
-   //map_detectorfile_str[3] = detector_directory+"cov_Recomb2.root";
+   map_detectorfile_str[3] = detector_directory+"cov_Recomb2.root";
    //map_detectorfile_str[4] = detector_directory+"cov_SCE.root";
         //map_detectorfile_str[5] = detector_directory+"cov_WMdEdx.root";
    //map_detectorfile_str[6] = detector_directory+"cov_WMThetaXZ.root";
