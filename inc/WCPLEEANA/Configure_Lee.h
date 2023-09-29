c074901b8ff1f73b656de46efb7c41d00d038e16
eyandel <eyandel16@gmail.com>
2023-09-29 15:56:36 -0500

    lee 0

--- inc/WCPLEEANA/Configure_Lee.h
+++ inc/WCPLEEANA/Configure_Lee.h
@@ -63,7 +63,7 @@ namespace config_Lee
   bool flag_syst_time = true;
 
   //double Lee_strength_for_outputfile_covariance_matrix = 1;
-  double Lee_strength_for_outputfile_covariance_matrix = 2.0;
+  double Lee_strength_for_outputfile_covariance_matrix = 0.0;
 
   double Lee_Np_strength_for_outputfile_covariance_matrix = 0;
   double Lee_0p_strength_for_outputfile_covariance_matrix = 0;
@@ -72,7 +72,7 @@ namespace config_Lee
 
   ////////// goodness of fit
 
-  double Lee_strength_for_GoF         = 2;
+  double Lee_strength_for_GoF         = 0;
 
   double Lee_Np_strength_for_GoF      = 0;
   double Lee_0p_strength_for_GoF      = 0;
