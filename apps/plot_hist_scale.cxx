3e4a2e8404b6fd072859d7bac1528edbd60e2545
eyandel <eyandel16@gmail.com>
2023-10-10 14:50:10 -0500

    don't draw all mc

--- apps/plot_hist_scale.cxx
+++ apps/plot_hist_scale.cxx
@@ -1615,7 +1615,7 @@ int main( int argc, char** argv )
 
         hmc->Sumw2();
         hmc->Scale(scalePOT);
-        hmc->Draw("hist");
+        //hmc->Draw("hist");
         hmc->GetYaxis()->SetTitle("Event counts");
         hmc->GetYaxis()->SetTitleSize(0.05);
         hmc->GetYaxis()->SetTitleFont(132);
