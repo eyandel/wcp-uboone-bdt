7c8070b9d19ea0fe9df38d2a81f36ba7695a93bc
eyandel <eyandel16@gmail.com>
2023-11-11 14:21:47 -0600

    reco enu

--- apps/plot_hist.cxx
+++ apps/plot_hist.cxx
@@ -1585,9 +1585,9 @@ int main( int argc, char** argv )
         //gratio_mc[obschannel-1]->GetXaxis()->SetTitle("Reco Shower Energy [MeV]");
         //gratio_mc[obschannel-1]->GetXaxis()->SetTitle("Median dE/dx (0-4 cm) [MeV/cm]");
         //gratio_mc[obschannel-1]->GetXaxis()->SetTitle("Reco Shower Cosine Angle");
-        gratio_mc[obschannel-1]->GetXaxis()->SetTitle("Number of Tracks");
+        //gratio_mc[obschannel-1]->GetXaxis()->SetTitle("Number of Tracks");
         //gratio_mc[obschannel-1]->GetXaxis()->SetTitle("#nu_{e}CC BDT Score");
-        //gratio_mc[obschannel-1]->GetXaxis()->SetTitle("Reco Neutrino Energy [MeV]");
+        gratio_mc[obschannel-1]->GetXaxis()->SetTitle("Reco Neutrino Energy [MeV]");
         //gratio_mc[obschannel-1]->GetXaxis()->SetTitle("NC#pi^{0} BDT Score");
         //gratio_mc[obschannel-1]->GetXaxis()->SetTitle("Reco #pi^{0} Mass [MeV]");
         //gratio_mc[obschannel-1]->GetXaxis()->SetTitle("Number of Showers");
