ef8c5b989c0edbfc07ff17170758f59798b2a0c9
eyandel <eyandel16@gmail.com>
2023-11-29 19:03:15 -0600

    test scaling for shifted cut

--- apps/plot_hist.cxx
+++ apps/plot_hist.cxx
@@ -1582,8 +1582,8 @@ int main( int argc, char** argv )
         }
         else*/ 
         //Erin 
-        //gratio_mc[obschannel-1]->GetXaxis()->SetTitle("Reco Shower Energy [MeV]");
-        gratio_mc[obschannel-1]->GetXaxis()->SetTitle("Median dE/dx (0-4 cm) [MeV/cm]");
+        gratio_mc[obschannel-1]->GetXaxis()->SetTitle("Reco Shower Energy [MeV]");
+        //gratio_mc[obschannel-1]->GetXaxis()->SetTitle("Median dE/dx (0-4 cm) [MeV/cm]");
         //gratio_mc[obschannel-1]->GetXaxis()->SetTitle("Reco Shower Cosine Angle");
         //gratio_mc[obschannel-1]->GetXaxis()->SetTitle("#nu_{e} CC Background BDT Score");
         //gratio_mc[obschannel-1]->GetXaxis()->SetTitle("Number of Tracks");
