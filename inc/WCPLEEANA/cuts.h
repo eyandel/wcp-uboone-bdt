d206665a1630b370898e664cfc549ca71625072b
eyandel <eyandel16@gmail.com>
2024-03-22 15:11:27 -0500

    normal 0p (35 mev) with run 1 c2 extbnb

--- inc/WCPLEEANA/cuts.h
+++ inc/WCPLEEANA/cuts.h
@@ -186,7 +186,7 @@ bool LEEana::is_true_0p(PFevalInfo& pfeval){
     for(size_t i=0; i<pfeval.truth_Ntrack; i++){
       if(pfeval.truth_mother[i] != 0) continue;
       if(pfeval.truth_pdg[i] != 2212) continue;
-      //if(pfeval.truth_startMomentum[i][3] - 0.938272 < 0.035) continue; //Erin: CHANGE, no proton threshold
+      if(pfeval.truth_startMomentum[i][3] - 0.938272 < 0.035) continue; //Erin: CHANGE, no proton threshold
       return false;
     }
   return true;
@@ -9007,8 +9007,8 @@ bool LEEana::is_0p(TaggerInfo& tagger_info, KineInfo& kine, PFevalInfo& pfeval){
       for(size_t i=0; i<kine.kine_energy_particle->size(); i++)
       {
           int pdgcode = kine.kine_particle_type->at(i);
-          //if(abs(pdgcode)==2212 && kine.kine_energy_particle->at(i)>35) Nproton++; // KE threshold: 50 MeV, 1.5 cm?
-          if(abs(pdgcode)==2212 && kine.kine_energy_particle->at(i)>0) Nproton++; //Erin: CHANGE, "actual 0p" aka no 35 MeV threshold
+          if(abs(pdgcode)==2212 && kine.kine_energy_particle->at(i)>35) Nproton++; // KE threshold: 50 MeV, 1.5 cm?
+          //if(abs(pdgcode)==2212 && kine.kine_energy_particle->at(i)>0) Nproton++; //Erin: CHANGE, "actual 0p" aka no 35 MeV threshold
           if(abs(pdgcode)==211 && kine.kine_energy_particle->at(i)>10) Npion++; // KE threshold: 10 MeV
       }
       if(Nproton==0) flag = true;
