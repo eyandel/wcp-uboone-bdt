5201d5192c569413dbc3664a18695306f25d3b74
Erin Yandel <eyandel16@gmail.com>
2024-08-22 16:56:05 -0500

    play with sizes

--- apps/plot_hist_sp_paper.cxx
+++ apps/plot_hist_sp_paper.cxx
@@ -1730,7 +1730,7 @@ int main( int argc, char** argv )
         //hack end
 
         gratio_mc[obschannel-1]->GetXaxis()->SetTitleSize(0.13);
-        gratio_mc[obschannel-1]->GetXaxis()->SetLabelSize(0.13);
+        gratio_mc[obschannel-1]->GetXaxis()->SetLabelSize(0.12);
         gratio_mc[obschannel-1]->GetXaxis()->SetTitleFont(22);//132);
         gratio_mc[obschannel-1]->GetXaxis()->SetLabelFont(22);//132);
         gratio_mc[obschannel-1]->GetYaxis()->SetTitleFont(22);//132);
