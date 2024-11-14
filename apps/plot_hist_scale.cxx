ff7580711b6b922e8fbcf337611d10d44036ac35
Erin Yandel <eyandel16@gmail.com>
2024-11-14 12:10:56 -0600

    once again scale

--- apps/plot_hist_scale.cxx
+++ apps/plot_hist_scale.cxx
@@ -445,7 +445,7 @@ int main( int argc, char** argv )
         //pad1->Draw();
         //pad2->Draw();
         hstack[obschannel-1] = new THStack(Form("hs%d", obschannel),"");
-        legend[obschannel-1] = new TLegend(0.45, 0.59, 0.9, 0.94);
+        legend[obschannel-1] = new TLegend(0.4, 0.59, 0.88, 0.96);
         legend3[obschannel-1] = new TLegend(0.55, 0.59, 0.85, 0.94);
         TH1F* hdata = (TH1F*)map_obsch_histos[obschannel].at(0)->Clone("hdata");
         TH1F* hdata_orignal = (TH1F*)map_obsch_histos[obschannel].at(0)->Clone("hdata_original");
@@ -1206,7 +1206,7 @@ int main( int argc, char** argv )
         //hdata->GetYaxis()->SetRangeUser(-0.02*excessymax, 2.7*excessymax);
         if (excessymin > 0.0) excessymin = 0.0;
         //hdata->GetYaxis()->SetRangeUser(17.0*excessymin - 0.02*excessymax, 2.7*excessymax);
-        hdata->GetYaxis()->SetRangeUser(30.0*excessymin - 0.02*excessymax, 4.0*excessymax);
+        hdata->GetYaxis()->SetRangeUser(30.0*excessymin - 0.08*excessymax, 3.5*excessymax);
         //hdata->GetXaxis()->SetRange(1, hdata->GetNbinsX() + 1);
 
         hdata->SetLineColor(kWhite);
