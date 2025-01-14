fc64f965e3e37911a57bbae2dbb2a6246b83cdd8
Erin Yandel <eyandel16@gmail.com>
2025-01-14 16:58:33 -0700

    fix legend

--- apps/plot_hist_sp_paper.cxx
+++ apps/plot_hist_sp_paper.cxx
@@ -944,7 +944,7 @@ int main( int argc, char** argv )
         if (flag_move == 1){
             legend[obschannel-1]->SetX1(0.14); // New x1 position
             legend[obschannel-1]->SetX2(0.8); // New x2 position
-            legend[obschannel-1]->SetY1(0.85); // New y1 position
+            legend[obschannel-1]->SetY1(0.48); // New y1 position
             legend[obschannel-1]->SetY2(0.9); // New y2 position
         }
         legend[obschannel-1]->SetFillStyle(0);
